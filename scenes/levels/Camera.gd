7266d4b4745b13dc59980cc4123a6eebb942a550
Tanel Marran <marran.tanel@gmail.com>
2021-10-02 16:59:00 +0300

    [player] adds camera

---
+++ scenes/levels/Camera.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+onready var player = get_node("../Player")
+onready var tilemap: TileMap = get_node("../Tilemap")
+onready var camera: Camera2D = get_node("Camera2D")
+
+func _ready():
+	var bounds: Rect2 = tilemap.get_used_rect()
+	
+	bounds.position *= 8
+	bounds.size *= 8
+	
+	
+	var leftLimit: float = bounds.position.x
+	var rightLimit: float = bounds.position.x + bounds.size.x
+	var topLimit: float = bounds.position.y
+	var bottomLimit: float = bounds.position.y + bounds.size.y
+	
+	print(leftLimit, ' ', rightLimit)
+	print(topLimit, ' ', bottomLimit)
+	
+	camera.limit_left = leftLimit
+	camera.limit_right = rightLimit
+	camera.limit_top = topLimit
+	camera.limit_bottom = bottomLimit
+
+func _process(delta):
+	position.x = player.position.x
+	position.y = player.position.y
