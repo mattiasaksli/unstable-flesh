305881add537826dda911791bff28f86fc1fe9da
Tanel Marran <marran.tanel@gmail.com>
2021-10-03 13:44:09 +0300

    [polish] adds flesh sound to lose ui

--- scripts/ui_animation_player.gd
+++ scripts/ui_animation_player.gd
@@ -4,5 +4,5 @@ extends AnimationPlayer
 func _ready():
 	play("FadeIn")
 	yield(self, "animation_finished")
-	yield(get_tree().create_timer(3), "timeout")
+	yield(get_tree().create_timer(6), "timeout")
 	get_tree().quit()
