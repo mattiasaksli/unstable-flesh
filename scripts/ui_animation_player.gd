23e80cd2eef37cba6b412eefc78923cd42f00ed2
Mattias Aksli <mattias.aksli@gmail.com>
2021-10-03 15:04:42 +0300

    Fix initial camera position and end screens

--- scripts/ui_animation_player.gd
+++ scripts/ui_animation_player.gd
@@ -6,3 +6,8 @@ func _ready():
 	yield(self, "animation_finished")
 	
 	get_tree().change_scene_to(load("res://scenes/title_screen.tscn"))
+
+
+func _input(event : InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
